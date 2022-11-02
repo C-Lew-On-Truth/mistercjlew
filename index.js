f78d8de9dfa335132334f6054e489aa99bd794d0
Clayton Lewis <lewisclayton89@gmail.com>
2022-11-02 19:35:02 -0400

    my-interest

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(window.innerWidth)
+
+const box1 = document.querySelector(".box-1");
+
+box1.onclick = () => {
+    console.log("box seen")
+    window.location.href = "my-interest.html"
+}
+
+
---
+++ js-files/nav.js
@@ -0,0 +1,12 @@
+let navBar = {
+    mainBar: `
+    <nav class="nav-bar">
+    <a href="#about">Who Am I</a>
+    <a href="#credit">Creditability</a>
+    <a href="#projects" href="#videos">Projects</a>
+    <a href="#contact">Contact Me</a>
+  </nav>
+    `
+}
+
+documen
