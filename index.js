2852ed2a85a8f34b163dc56f6e92375d29d4db0a
Clayton Lewis <lewisclayton89@gmail.com>
2022-11-07 21:07:13 -0500

    thats it for now

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log(window.innerWidth)
 
 const box1 = document.querySelector(".box-1");
+const box2 = document.querySelector(".box-2");
 
 box1.onclick = () => {
     console.log("box seen")
     window.location.href = "my-interest.html"
 }
+
+box2.onclick = () => {
+    console.log("box seen")
+    window.location.href = "my-ability.html"
+}
+
 
 
