7455e40652561e93b7b641e36c7262995c71ee01
Clayton Lewis <lewisclayton89@gmail.com>
2022-11-29 14:39:27 -0500

    additional pages added

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ console.log(window.innerWidth)
 
 const box1 = document.querySelector(".box-1");
 const box2 = document.querySelector(".box-2");
+const box3 = document.querySelector(".box-3");
+const box4 = document.querySelector(".box-4");
+const box5 = document.querySelector(".box-5");
+const goHome = document.querySelector(".go-home")
 
 box1.onclick = () => {
-    console.log("box seen")
     window.location.href = "my-interest.html"
 }
 
 box2.onclick = () => {
-    console.log("box seen")
     window.location.href = "my-ability.html"
 }
+
+box3.onclick = () => {
+    window.location.href = "contact-me.html"
+}
+
+box4.onclick = () => {
+    window.location.href = "videos.html"
+}
+
+box5.onclick = () => {
+    window.location.href = "web-projects.html"
+}
+
+goHome.onclick = () => {
+    console.log("box seen")
+    window.location.href = "index.html"
+}
+
+
+
 
 
 
