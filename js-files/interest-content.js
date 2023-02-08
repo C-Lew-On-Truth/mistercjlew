995cb6501ec604935485bcb1665195e47dd84e27
Clayton <clayton@Claytons-Air-2.lan>
2023-02-08 17:43:49 -0500

    my-interest page

---
+++ js-files/interest-content.js
@@ -0,0 +1,5 @@
+let entries = {
+    family:"FAMILY"
+};
+
+export { entries };
--- js-files/my-interest.js
+++ js-files/my-interest.js
@@ -1,17 +1,14 @@
+import "./interest-content";
+
 console.log(window.innerWidth);
+
 
 const interest = document.querySelector(".interest");
 let gifHolder = document.querySelector(".gif-holder");
+let content = document.querySelector(".content")
 
 interest.onmouseover = function (ev) {
   let target = ev.target.textContent;
-  /*
-  if (target === "Family") {
-    console.log(target);
-    gifHolder.style.backgroundImage = "none"
-    gifHolder.innerHTML = "FAMILY"
-  }
-*/
   switch (target) {
     case "Family":
       gifHolder.style.backgroundImage = "none";
@@ -25,10 +22,10 @@ interest.onmouseover = function (ev) {
       gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Hockey`;
       break;
-      case "Skating":
-        gifHolder.style.backgroundImage = "none";
-        gifHolder.innerHTML = `Skate or die!`;
-        break;
+    case "Skating":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Skate or die!`;
+      break;
     case "Music":
       gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Music`;
@@ -68,4 +65,62 @@ interest.onmouseover = function (ev) {
 interest.onmouseleave = function (ev) {
   gifHolder.innerHTML = "";
   gifHolder.style.backgroundImage = "url(../media/muppet.gif)";
+  content.style.display = "none"
+};
+
+interest.onclick = function (ev) {
+  let target = ev.target.textContent;
+ content.style.display = "block"
+  switch (target) {
+    case "Family":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.textContent = `FAMILY`;
+      content.textContent = entries[family]
+      break;
+    case "Coaching":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Coaching`;
+      break;
+    case "Hockey":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Hockey`;
+      break;
+    case "Skating":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Skate or die!`;
+      break;
+    case "Music":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Music`;
+      break;
+    case "Coding":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Coding`;
+      break;
+    case "Cooking":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Cooking`;
+      break;
+    case "Delicious Cusine":
+      console.log(target);
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `FOOD!!!`;
+      break;
+    case "Basketball":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Balling!`;
+      break;
+    case "Good Company":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `Do it for your dawgs!!!`;
+      break;
+    case "Comedy":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `I'm rolling dawg!`;
+      break;
+    case "Peace and Love":
+      gifHolder.style.backgroundImage = "none";
+      gifHolder.innerHTML = `The only way to save the world`;
+      break;
+  }
 };
