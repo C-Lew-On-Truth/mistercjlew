764fd5255713481377eb70cc1b479e064b978f92
Clayton <clayton@Claytons-MacBook-Air-2.local>
2023-02-14 11:59:05 -0500

    my-interest page sorted out??

--- js-files/my-interest.js
+++ js-files/my-interest.js
@@ -1,36 +1,41 @@
 //gimport "./interest-content";
-/*
+
 console.log(window.innerWidth);
 
 
 const interest = document.querySelector(".interest");
 let gifHolder = document.querySelector(".gif-holder");
 let content = document.querySelector(".content")
-let giffyImage = document.querySelector(".giffy-image")
 
 interest.onmouseover = function (ev) {
   let target = ev.target.textContent;
   switch (target) {
     case "Family":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.textContent = `FAMILY`;
       break;
     case "Coaching":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Coaching`;
       break;
     case "Hockey":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Hockey`;
       break;
     case "Skating":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Skate or die!`;
       break;
     case "Music":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Music`;
       break;
     case "Coding":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Coding`;
       break;
     case "Cooking":
-
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Cooking`;
       break;
     case "Delicious Cusine":
@@ -39,23 +44,26 @@ interest.onmouseover = function (ev) {
       gifHolder.innerHTML = `FOOD!!!`;
       break;
     case "Basketball":
-
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Balling!`;
       break;
     case "Good Company":
-
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `Do it for your dawgs!!!`;
       break;
     case "Comedy":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `I'm rolling dawg!`;
       break;
     case "Peace and Love":
+      gifHolder.style.backgroundImage = "none";
       gifHolder.innerHTML = `The only way to save the world`;
       break;
   }
 };
 
-interest.onmouseleave = function (ev) {
+interest.onmouseleave = function () {
+  gifHolder.style.backgroundImage = "url('../media/muppet.gif')"
   gifHolder.innerHTML = "";
   content.style.display = "none"
 };
@@ -115,4 +123,3 @@ interest.onclick = function (ev) {
       break;
   }
 };
-*/
