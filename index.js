09e9f79e8d7b52c4e1170498c7245496eec61410
Clayton Lewis <lewisclayton89@gmail.com>
2023-06-05 17:11:58 -0400

    new style

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
-console.log(window.innerWidth)
+console.log(window.innerWidth);
 
-const box1 = document.querySelector(".box-1");
-const box2 = document.querySelector(".box-2");
-const box3 = document.querySelector(".box-3");
-const box4 = document.querySelector(".box-4");
-const box5 = document.querySelector(".box-5");
-const goHome = document.querySelector(".go-home")
+const navBar = document.querySelector(".nav-bar");
+let content = document.querySelector(".content");
 
-box1.onclick = () => {
-    window.location.href = "my-interest.html"
-}
-
-box2.onclick = () => {
-    window.location.href = "my-ability.html"
-}
-
-box3.onclick = () => {
-    window.location.href = "contact-me.html"
-}
-
-box4.onclick = () => {
-    window.location.href = "videos.html"
-}
-
-box5.onclick = () => {
-    window.location.href = "web-projects.html"
-}
-
-goHome.onclick = () => {
-    console.log("box seen")
-    window.location.href = "index.html"
-}
-
-
-
-
-
-
+navBar.onclick = (ev) => {
+  let target = ev.target.textContent;
+  switch (target) {
+    case "ABOUT":
+      content.src = "about.html";
+      break;
+    case "EXPERIENCE":
+      content.src = "experience.html";
+      break;
+    case "CONTACT":
+      content.src = "contact-me.html";
+      break;
+  }
+};
