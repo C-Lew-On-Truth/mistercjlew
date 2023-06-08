791e7206d3161d82dd6f6d6e8e2127bf46b7adb5
Clayton Lewis <lewisclayton89@gmail.com>
2023-06-08 10:33:16 -0400

    profile links added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log(window.innerWidth);
 
 const navBar = document.querySelector(".nav-bar");
+const proBar = document.querySelector(".pro-bar");
 let content = document.querySelector(".content");
 
 content.src = "contact.html"
@@ -16,6 +17,21 @@ navBar.onclick = (ev) => {
       break;
     case "CONTACT":
       content.src = "contact.html";
+      break;
+  }
+};
+
+proBar.onclick = (ev) => {
+  let target = ev.target.textContent;
+  switch (target) {
+    case "GitHub":
+      window.open('https://github.com/C-Lew-On-Truth', '_blank');
+      break;
+    case "Linkedin":
+      window.open('https://www.linkedin.com/in/clayton-lewis-1a604b92/', '_blank');
+      break;
+    case "CodePen":
+      window.open('https://codepen.io/c-lew-on-truth', '_blank');
       break;
   }
 };
