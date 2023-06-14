f4beaf6591e61c5991d34fa9cadca94bee52c480
Clayton Lewis <lewisclayton89@gmail.com>
2023-06-14 17:01:35 -0400

    form third party addition

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const navBar = document.querySelector(".nav-bar");
 const proBar = document.querySelector(".pro-bar");
 let content = document.querySelector(".content");
 
-content.src = "contact.html"
+content.src = "about.html"
 
 navBar.onclick = (ev) => {
   let target = ev.target.textContent;
