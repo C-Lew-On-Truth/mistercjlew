827d5a6d016eb6f416f780cb455dc4fb1182b735
Clayton Lewis <lewisclayton89@gmail.com>
2023-06-16 18:33:12 -0400

    about me rwd issues

--- js-files/about.js
+++ js-files/about.js
@@ -1,6 +1,22 @@
-const bitmojiPic = document.querySelector(".bitmoji-pic")
-bitmojiPic .onclick = () => {
+let pic = document.querySelector(".pic")
+let thankYouHolder = document.querySelector(".thank-message")
+let thankMessage= document.querySelector(".thank-message p")
+let intro = document.querySelector(".intro")
+
+/*
+setTimeout(() => {
+  aboutHeader.innerHTML= "Im Clayton Lewis"
+  aboutHeader.style.opacity = 1;
+
+},1500)
+*/
+
+setTimeout(() => {
+  thankMessage.style.opacity =  1
+}, 1000)
+
   setTimeout(() => {
-    about.style.color = "red"
-  }, 1000)
-}
+    thankYouHolder.style.top =  "-500px"
+    pic.style.bottom = 0
+  }, 3000)
+  
