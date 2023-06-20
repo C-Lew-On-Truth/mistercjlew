addcdd059f7476665df3c0649380eb5b50014398
Clayton Lewis <lewisclayton89@gmail.com>
2023-06-20 10:53:43 -0400

    home page style

--- js-files/about.js
+++ js-files/about.js
@@ -1,22 +1,24 @@
-let pic = document.querySelector(".pic")
-let thankYouHolder = document.querySelector(".thank-message")
-let thankMessage= document.querySelector(".thank-message p")
-let intro = document.querySelector(".intro")
-
-/*
-setTimeout(() => {
-  aboutHeader.innerHTML= "Im Clayton Lewis"
-  aboutHeader.style.opacity = 1;
-
-},1500)
-*/
+let pic = document.querySelector(".pic");
+let thankYouHolder = document.querySelector(".thank-message");
+let thankMessage = document.querySelector(".thank-message p");
+let intro = document.querySelector(".intro");
 
 setTimeout(() => {
-  thankMessage.style.opacity =  1
-}, 1000)
+  intro.style.transition = "1s";
+  intro.style.opacity = 0;
+}, 1500);
 
-  setTimeout(() => {
-    thankYouHolder.style.top =  "-500px"
-    pic.style.bottom = "-2px"
-  }, 3000)
-  
+setTimeout(() => {
+  intro.style.transition = "2s";
+  intro.style.opacity = 1;
+  intro.textContent = "I'm Clayton Lewis";
+}, 3000);
+
+setTimeout(() => {
+  thankMessage.style.opacity = 1;
+}, 1000);
+
+setTimeout(() => {
+  thankYouHolder.style.top = "-500px";
+  pic.style.bottom = "-2px";
+}, 3000);
